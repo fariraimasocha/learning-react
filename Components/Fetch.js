351135bc14b6ece46514a3f7cadbdf0d9e93dc57
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-03-17 14:32:08 +0200

    feat: implement user fetching with pagination and add Card and Button components

--- Components/Fetch.js
+++ Components/Fetch.js
@@ -1,16 +1,30 @@
-import React from "react";
+import { useState } from "react";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+  CardContent,
+} from "./ui/card";
+import { Button } from "./ui/button";
+import Image from "next/image";
 
 export default function Fetch() {
-  const fetchData = () => {
-    const response = axios.get("https://randomuser.me/api?results=1");
-    const results = response.then((res) => res.data);
-    return results;
+  const [page, setPage] = useState(1);
+
+  const fetchData = async () => {
+    const response = await axios.get(`https://randomuser.me/api?page=${page}`);
+    return response.data;
+  };
+
+  const handleNext = () => {
+    setPage((prev) => prev + 1);
   };
 
   const { data, isPending, isError, error } = useQuery({
-    queryKey: [fetchData],
+    queryKey: ["users", page],
     queryFn: fetchData,
   });
 
@@ -18,5 +32,34 @@ export default function Fetch() {
 
   if (isError) return <span>Error: {error.message}</span>;
 
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div className="max-w-2xl mt-10 px-10">
+      {data.results.map((user) => (
+        <Card key={user.email}>
+          <CardHeader>
+            <CardTitle>
+              <div className="flex gap-1.5">
+                <span className="font-bold">{user.name.first}</span>
+                <span className="font-bold">{user.name.last}</span>
+              </div>
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Image
+              priority
+              src={user.picture.thumbnail}
+              width={50}
+              height={50}
+              alt="profile"
+            />
+          </CardContent>
+          <CardFooter>
+            <Button variant="outline" onClick={handleNext}>
+              Next
+            </Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
 }
