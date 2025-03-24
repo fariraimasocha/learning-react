858d5964baf0455730478efc6452ca977a68acfc
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-03-24 09:13:44 +0200

    refactor: remove unused import of Interview component in page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 import Intermediate from "@/Components/Intermediate";
 
 // import Interview from "@/Components/Interview";
