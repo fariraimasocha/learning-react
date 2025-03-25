05c708d3cdd53714c1c22ff350e8fc2f0aa51763
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-03-25 15:59:38 +0200

    refactor: simplify Interview component and update MainPage imports

--- app/page.js
+++ app/page.js
@@ -1,18 +1,18 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Intermediate from "@/Components/Intermediate";
+// import Intermediate from "@/Components/Intermediate";
 
-// import Interview from "@/Components/Interview";
+import Interview from "@/Components/Interview";
 // import Fetch from "@/Components/Fetch";
 
 export default function MainPage() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <Intermediate />
+      {/* <Intermediate /> */}
       {/* <Fetch /> */}
-      {/* <Interview /> */}
+      <Interview />
     </QueryClientProvider>
   );
 }
