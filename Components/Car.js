813252c324df07c0e0c09a3d9034a5165a554772
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-03-27 13:45:58 +0200

    feat: add Prettier for code formatting and update Car component layout

--- Components/Car.js
+++ Components/Car.js
@@ -8,7 +8,7 @@ export default function Car({ color, model }) {
         <div className='p-4 bg-gray-100'>
 
             {
-                far <= 10 ? <p>Fatso</p> : <div>   <h1>Car</h1>
+                far <= 10 ? <p>Fatso</p> : <div><h1>Car</h1>
                     <p>Color: {color}</p>
                     <p>Model: {model}</p>
                     <Button variant='outline' onClick={() => {
