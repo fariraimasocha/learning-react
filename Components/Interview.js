14f4abbe55a033f72ef4ab9dab7d3d84f13a9bd1
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-03-31 14:11:49 +0200

    feat: implement coffee data fetching in Interview component using React Query

--- Components/Interview.js
+++ Components/Interview.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 export default function Interview() {
-  return <div>Interview</div>;
+  const fetchCoffee = () => {
+    const response = axios.get("https://coffee.alexflipnote.dev");
+    return response;
+  };
+
+  const { data, isPending, error, isError } = useQuery({
+    queryKey: ["coffee"],
+    queryFn: fetchCoffee,
+  });
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+  return <div>{JSON.stringify(data)}</div>;
 }
