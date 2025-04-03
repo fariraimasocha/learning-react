640ef3cfa7f5749a0ba3aae6701c93ee4a3a8579
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-04-03 11:51:55 +0200

    feat: render a list of cars in Scrimba component

--- Components/Scrimba.js
+++ Components/Scrimba.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Card } from './ui/card'
 
 export default function Scrimba() {
+
+const cars = [
+  { id: 1, name: "Ford", model: "Mustang" },
+  { id: 2, name: "Chevy", model: "Camaro" },
+  { id: 3, name: "Dodge", model: "Charger" }
+]
+
+
   return (
-    <div>Scrimba</div>
+    <div className='px-20 py-20'>
+    
+        <h1>Render list</h1>
+        {
+            cars.map((cars) => {
+                return (
+                <Card key={cars.id} className="w-[300px] h-[200px] m-2 px-2">
+                    <h1 className='text-lg'>{cars.name}</h1>
+                    <p className='text-lg'>{cars.model}</p>
+                </Card>
+                )
+            })
+        }
+
+    </div>
   )
 }
