ebb95deeb00088bbbb5c2df06f79af1a088649d5
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-05-01 20:11:21 +0200

    fix: uncomment Fetch component and comment out UseMemo in MainPage

--- app/page.js
+++ app/page.js
@@ -3,19 +3,19 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import Intermediate from "@/Components/Intermediate";
 // import Interview from '@/Components/Interview';
-// import Fetch from "@/Components/Fetch";
+import Fetch from "@/Components/Fetch";
 // import Scrimba from '@/Components/Scrimba';
-import UseMemo from '@/Components/useMemo';
+// import UseMemo from '@/Components/useMemo';
 
 export default function MainPage() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       {/* <Intermediate /> */}
-      {/* <Fetch /> */}
+      <Fetch />
       {/* <Interview /> */}
       {/* <Scrimba /> */}
-      <UseMemo />
+      {/* <UseMemo /> */}
     </QueryClientProvider>
   );
 }
