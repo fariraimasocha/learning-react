eb61f34752700429769ab2f759105819f441a7c0
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-05-12 20:17:31 +0200

    fix: update return statement in Tenary component to use Pedro and Not_Pedro components

--- Components/Test.js
+++ Components/Test.js
@@ -12,5 +12,21 @@ export default function Tenary() {
   let name = age > 9 ? 'pedro' : 'not Pedro';
   console.log(name);
 
-  return age > 9 ? <div>pedro</div> : <div>Not Pedro</div>;
+  return age > 9 ? <Pedro /> : <Not_Pedro />;
 }
+
+export const Pedro = async () => {
+  return (
+    <div>
+      <h1>Pedro</h1>
+    </div>
+  );
+};
+
+export const Not_Pedro = async () => {
+  return (
+    <div>
+      <h1>Not Pedro</h1>
+    </div>
+  );
+};
