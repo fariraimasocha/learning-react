c27045558189193510fd9e302ceb1ce6f8adc33c
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-05-13 11:54:39 +0200

    feat: implement fetchDomain function to retrieve available domains

--- Components/BuyDomain.js
+++ Components/BuyDomain.js
@@ -5,12 +5,39 @@ import { Input } from './ui/input';
 import { Button } from './ui/button';
 import { Card } from './ui/card';
 import { Label } from './ui/label';
+import { useQuery } from '@tanstack/react-query';
 
 export default function BuyDomain() {
   const [domain, setDomain] = useState('');
 
   const BaseUrl = 'api.ote-godaddy.com';
   const CustomerId = '658344786';
+
+  const fetchDomain = async () => {
+    const response = await fetch(`${BaseUrl}/v1/domains/available`, {
+      method: 'GET',
+      headers: {
+        Authorization: `sso-key ${CustomerId}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        domain: domain,
+        checkType: 'FAST',
+        forTransfer: false,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return data;
+  };
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['domain', domain],
+    queryFn: () => fetchDomain,
+  });
 
   return (
     <div className="flex justify-center items-center h-screen">
@@ -20,8 +47,12 @@ export default function BuyDomain() {
         </div>
         <Card className="w-[700] px-5 mt-5">
           <Label htmlFor="email">Domain Aquistion</Label>
-          <Input placeholder="Enter domain to buy" />
-          <Button>Search</Button>
+          <Input
+            value={domain}
+            onChange={(e) => setDomain(e.target.value)}
+            placeholder="Enter domain to buy"
+          />
+          <Button>Buy</Button>
         </Card>
       </div>
     </div>
