13ef1e854aa575a9b74373ee2f09f92acab31634
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-05-20 11:08:29 +0200

    feat: add GET endpoint to fetch account balance from Name.com API

--- app/api/name/balance/route.js
+++ app/api/name/balance/route.js
@@ -1,19 +1,22 @@
 import { NextResponse } from 'next/server';
 
-const username = 'pateltanaka';
+const username = 'pateltanaka-test';
 const token = process.env.NAMEDOTCOM_API_KEY;
 
 export async function GET(request) {
   const credentials = Buffer.from(`${username}:${token}`).toString('base64');
 
   try {
-    const response = await fetch('https://api.dev.name.com/v4/domains', {
-      method: 'GET',
-      headers: {
-        Authorization: `Basic ${credentials}`,
-        'Content-Type': 'application/json',
-      },
-    });
+    const response = await fetch(
+      'https://api.dev.name.com/v4/accountinfo/balance',
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Basic ${credentials}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
 
     if (!response.ok) {
       throw new Error('Network response was not ok');
