2bdf16288a3e6db1e9d1f05dcc1d0732d1c2b52a
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-05-20 12:52:20 +0200

    feat: implement POST endpoint to set nameservers for a domain

--- app/api/name/setNameservers/route.js
+++ app/api/name/setNameservers/route.js
@@ -5,4 +5,35 @@ const token = process.env.NAMEDOTCOM_API_KEY;
 
 export async function GET(request) {
   const credentials = Buffer.from(`${username}:${token}`).toString('base64');
+
+  const { domain, nameservers } = await request.json();
+  try {
+    const response = await fetch(
+      `https://api.dev.name.com/v4/domains/${domain}/nameservers`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${credentials}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ nameservers }),
+      }
+    );
+
+    if (!response.ok) {
+      return NextResponse.json(
+        { error: 'Failed to set nameservers' },
+        { status: response.status }
+      );
+    }
+
+    const data = await response.json();
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    return NextResponse.json(
+      { error: 'Failed to fetch data' },
+      { status: 500 }
+    );
+  }
 }
