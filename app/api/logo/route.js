300ed7b418fd005ef267be4cd7352adcad5da1d3
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-05-21 18:39:58 +0200

    feat: implement GET route for fetching logos by userId

--- app/api/logo/route.js
+++ app/api/logo/route.js
@@ -2,7 +2,33 @@ import { NextResponse } from 'next/server';
 import Logo from '@/models/logo';
 import { connect } from '@/utils/connect';
 
-export default async function POST(req) {
+export async function GET(req) {
+  await connect();
+  const { searchParams } = new URL(req.url);
+  const userId = searchParams.get('userId');
+
+  try {
+    const logos = await Logo.find({ userId });
+
+    return NextResponse.json(
+      {
+        success: true,
+        data: logos,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: 'Error fetching logos',
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(req) {
   await connect();
   const { userId, url } = await req.json();
 
