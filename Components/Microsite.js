b0b1abc3a5314d62ce16d1568e56931ec8f38164
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-05-22 08:04:24 +0200

    feat: enhance logo fetching with improved API call and loading state management

--- Components/Microsite.js
+++ Components/Microsite.js
@@ -16,6 +16,7 @@ import { toast } from 'sonner';
 import { useRouter } from 'next/navigation';
 import { UploadDropzone } from '@/utils/uploadthing';
 import Image from 'next/image';
+import CircularSpinner from './Loading';
 
 export default function Microsite() {
   const router = useRouter();
@@ -38,11 +39,14 @@ export default function Microsite() {
     const fetchLogo = async () => {
       setIsLogoLoading(true);
       try {
-        const response = await fetch(`/api/logo/${userId}`);
+        const response = await fetch(`/api/logo?userId=${userId}`);
         const data = await response.json();
-        if (data && data.url) {
-          setLogoUrl(data.url);
+        if (data.success && data.data && data.data.length > 0) {
+          setLogoUrl(data.data[0].url);
+          console.log('Logo URL:', data.data[0].url);
           toast.success('Logo fetched successfully');
+        } else {
+          console.log('No logos found for this user');
         }
       } catch (error) {
         console.error('Error fetching logo:', error);
@@ -52,7 +56,7 @@ export default function Microsite() {
       }
     };
     fetchLogo();
-  })[userId];
+  }, [userId]);
 
   const handleSocialLinkChange = (platform, value) => {
     setSocialLinks((prevLinks) => ({
@@ -119,143 +123,162 @@ export default function Microsite() {
     }
   };
 
-  return (
-    <div className="flex justify-center items-center">
-      <div>
-        <Card className="w-[700] px-5 mt-5">
-          <CardHeader>
-            <CardTitle>Your Microsite</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="grid gap-4">
-              {/* Image Upload Section */}
-              <div className="mb-4">
-                <Label htmlFor="image" className="mb-2 block">
-                  Site Image
-                </Label>
-                {!imageUrl ? (
-                  <UploadDropzone
-                    endpoint="imageUploader"
-                    onClientUploadComplete={handleUploadComplete}
-                    onUploadError={(error) => {
-                      toast.error(`Upload error: ${error.message}`);
-                    }}
-                  />
-                ) : (
-                  <div className="flex flex-col gap-2 items-center">
-                    <div className="relative w-full h-48 overflow-hidden rounded-md">
-                      <Image
-                        src={imageUrl}
-                        alt="Site image"
-                        fill
-                        className="object-cover"
-                      />
-                    </div>
-                    <Button
-                      variant="outline"
-                      size="sm"
-                      onClick={() => setImageUrl('')}
-                    >
-                      Replace Image
-                    </Button>
-                  </div>
-                )}
-              </div>
-
-              <Label htmlFor="name">Name</Label>
-              <Input
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Enter microsite name"
-              />
-              <Label htmlFor="description">Description</Label>
-              <Textarea
-                id="description"
-                placeholder="Enter microsite description"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-              />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <div className="w-full flex flex-col gap-2">
-              <Label className="text-sm text-gray-500">Your Social Links</Label>
-              <Input
-                id="facebook-link"
-                value={socialLinks.facebook}
-                onChange={(e) =>
-                  handleSocialLinkChange('facebook', e.target.value)
-                }
-                placeholder="facebook"
-              />
-              <Input
-                id="twitter-link"
-                value={socialLinks.twitter}
-                onChange={(e) =>
-                  handleSocialLinkChange('twitter', e.target.value)
-                }
-                placeholder="twitter"
-              />
-            </div>
-          </CardFooter>
-          <Button
-            className="mx-5 mb-5"
-            onClick={handleSubmit}
-            type="submit"
-            disabled={isSubmitting || !name || !description || !imageUrl}
-          >
-            {isSubmitting ? 'Publishing...' : 'Publish'}
-          </Button>
-        </Card>
-        <Card className="mt-4 mb-4">
-          <CardHeader>
-            <CardTitle>Microsite Preview</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="flex flex-col">
-              {imageUrl && (
-                <div className="relative w-full h-48 mb-4 overflow-hidden rounded-md">
-                  <Image
-                    src={imageUrl}
-                    alt="Site image preview"
-                    fill
-                    className="object-cover"
-                  />
-                </div>
+  return isLogoLoading ? (
+    <>
+      <CircularSpinner size="medium" color="text-sidenav" />
+    </>
+  ) : (
+    <>
+      <div className="flex justify-center items-center">
+        <div>
+          <Card className="w-[700] px-5 mt-5">
+            <CardHeader>
+              <CardTitle>Your Microsite</CardTitle>
+              {logoUrl && (
+                <Image
+                  src={logoUrl}
+                  alt="Logo"
+                  width={100}
+                  priority
+                  height={100}
+                />
               )}
-              <h2 className="text-lg font-semibold">
-                {name || 'Your Site Name'}
-              </h2>
-              <p className="text-sm text-gray-500">
-                {description || 'Your site description will appear here'}
-              </p>
-            </div>
-          </CardContent>
-          <CardFooter>
-            <div className="w-full">
-              <Label className="text-sm text-gray-500 mb-2 block">
-                Social Links:
-              </Label>
-              <div className="flex flex-col gap-2">
-                {socialLinks.facebook && (
-                  <div className="text-sm">
-                    Facebook: {socialLinks.facebook}
-                  </div>
-                )}
-                {socialLinks.twitter && (
-                  <div className="text-sm">Twitter: {socialLinks.twitter}</div>
-                )}
-                {!socialLinks.facebook && !socialLinks.twitter && (
-                  <div className="text-sm text-gray-400">
-                    No social links added
-                  </div>
-                )}
+            </CardHeader>
+            <CardContent>
+              <div className="grid gap-4">
+                {/* Image Upload Section */}
+                <div className="mb-4">
+                  <Label htmlFor="image" className="mb-2 block">
+                    Site Image
+                  </Label>
+                  {!imageUrl ? (
+                    <UploadDropzone
+                      endpoint="imageUploader"
+                      onClientUploadComplete={handleUploadComplete}
+                      onUploadError={(error) => {
+                        toast.error(`Upload error: ${error.message}`);
+                      }}
+                    />
+                  ) : (
+                    <div className="flex flex-col gap-2 items-center">
+                      <div className="relative w-full h-48 overflow-hidden rounded-md">
+                        <Image
+                          src={imageUrl}
+                          alt="Site image"
+                          fill
+                          className="object-cover"
+                        />
+                      </div>
+                      <Button
+                        variant="outline"
+                        size="sm"
+                        onClick={() => setImageUrl('')}
+                      >
+                        Replace Image
+                      </Button>
+                    </div>
+                  )}
+                </div>
+
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Enter microsite name"
+                />
+                <Label htmlFor="description">Description</Label>
+                <Textarea
+                  id="description"
+                  placeholder="Enter microsite description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
               </div>
-            </div>
-          </CardFooter>
-        </Card>
+            </CardContent>
+            <CardFooter>
+              <div className="w-full flex flex-col gap-2">
+                <Label className="text-sm text-gray-500">
+                  Your Social Links
+                </Label>
+                <Input
+                  id="facebook-link"
+                  value={socialLinks.facebook}
+                  onChange={(e) =>
+                    handleSocialLinkChange('facebook', e.target.value)
+                  }
+                  placeholder="facebook"
+                />
+                <Input
+                  id="twitter-link"
+                  value={socialLinks.twitter}
+                  onChange={(e) =>
+                    handleSocialLinkChange('twitter', e.target.value)
+                  }
+                  placeholder="twitter"
+                />
+              </div>
+            </CardFooter>
+            <Button
+              className="mx-5 mb-5"
+              onClick={handleSubmit}
+              type="submit"
+              disabled={isSubmitting || !name || !description || !imageUrl}
+            >
+              {isSubmitting ? 'Publishing...' : 'Publish'}
+            </Button>
+          </Card>
+          <Card className="mt-4 mb-4">
+            <CardHeader>
+              <CardTitle>Microsite Preview</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-col">
+                {imageUrl && (
+                  <div className="relative w-full h-48 mb-4 overflow-hidden rounded-md">
+                    <Image
+                      src={imageUrl}
+                      alt="Site image preview"
+                      fill
+                      className="object-cover"
+                    />
+                  </div>
+                )}
+                <h2 className="text-lg font-semibold">
+                  {name || 'Your Site Name'}
+                </h2>
+                <p className="text-sm text-gray-500">
+                  {description || 'Your site description will appear here'}
+                </p>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <div className="w-full">
+                <Label className="text-sm text-gray-500 mb-2 block">
+                  Social Links:
+                </Label>
+                <div className="flex flex-col gap-2">
+                  {socialLinks.facebook && (
+                    <div className="text-sm">
+                      Facebook: {socialLinks.facebook}
+                    </div>
+                  )}
+                  {socialLinks.twitter && (
+                    <div className="text-sm">
+                      Twitter: {socialLinks.twitter}
+                    </div>
+                  )}
+                  {!socialLinks.facebook && !socialLinks.twitter && (
+                    <div className="text-sm text-gray-400">
+                      No social links added
+                    </div>
+                  )}
+                </div>
+              </div>
+            </CardFooter>
+          </Card>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
