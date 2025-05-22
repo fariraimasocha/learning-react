2a98219b793cdcf863172b282212073fb76b9446
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-05-22 11:22:40 +0200

    feat: implement GET and POST routes for product API with error handling

--- models/Product.js
+++ models/Product.js
@@ -3,6 +3,10 @@ import mongoose from 'mongoose';
 
 const ProductSchema = new mongoose.Schema(
   {
+    userId: {
+      type: String,
+      required: true,
+    },
     name: {
       type: String,
       required: true,
