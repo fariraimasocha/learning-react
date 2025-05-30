88996d189625e3efbc63b03ec594c30fd8baa075
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-05-30 13:07:01 +0200

    feat: add Tanstack component and integrate it into the Tanstack page

---
+++ app/tanstack/page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TanstackQuery from '@/Components/Tanstack';
+
+export default function Tanstack() {
+  return (
+    <>
+      <TanstackQuery />
+    </>
+  );
+}
