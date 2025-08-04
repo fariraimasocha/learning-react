45233729157afa771d5949d023e0c8b90b8e11c5
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-08-04 20:18:03 +0200

    wip
    resend fix

--- Components/Email.js
+++ Components/Email.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Button } from './ui/button';
 import { sendEmail } from '@/utils/email';
+import { toast } from 'sonner';
 
 export default function Email() {
   const handleSendEmail = async () => {
     const result = await sendEmail();
     if (result.success) {
-      alert('Email sent successfully!');
+      toast.success('Email sent successfully!');
+      console.log('Email data:', result.data);
     } else {
-      alert('Failed to send email: ' + result.error);
+      toast.error(`Error sending email: ${result.error}`);
+      console.error('Email error:', result.error);
     }
   };
 
   return (
-    <div>
-      <Button onClick={handleSendEmail}>Send Mail</Button>
+    <div className='min-h-screen flex justify-center items-center'>
+      <Button className="bg-gray-900 text-white hover:bg-gray-800 transition-all" onClick={handleSendEmail}>Send Mail</Button>
     </div>
   );
 }
