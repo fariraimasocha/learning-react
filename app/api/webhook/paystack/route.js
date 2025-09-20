0d6851a6dcb63ebf73c4767287c2d2f581a8b456
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2025-09-20 16:42:10 +0200

    feat: ✨ Add response to Paystack webhook route

--- app/api/webhook/paystack/route.js
+++ app/api/webhook/paystack/route.js
@@ -1,5 +1,7 @@
 import { verifyTrasaction } from '@/utils/paystack';
+import { NextResponse } from 'next/server';
 
 export async function POST(request) {
   console.log('Received Paystack webhook request', request);
+  return NextResponse.json({ message: 'Webhook received' }, { status: 200 });
 }
